722c48992ddd65eeca3c8f1abae5b851153c34b1
RomanJ55 <roman.jaro@web.de>
2020-12-31 21:31:37 +0100

    added "incheck" and "checkmate" parameters to King class, new images for kings(red-ish if king.incheck)

--- source/pieces/king.py
+++ source/pieces/king.py
@@ -5,13 +5,28 @@ class King(Piece):
     def __init__(self, x, y, player):
         super().__init__(x=x, y=y, player=player)
         self.type = 5
-        self.is_check = False
-        self.is_checkmate = False
+        self.incheck = False
+        self.checkmate = False
         self.set_image()
+
+    def is_incheck(self):
+        return True if self.incheck else False
+
+    def is_checkmate(self):
+        return True if self.checkmate else False
+
+    def put_incheck(self):
+        self.incheck = True
+
+    def put_outof_check(self):
+        self.incheck = False
 
     def set_image(self):
         color_num = 0 if self.player == "white" else 1
-        self.image = f"img\\{color_num}0{self.type}.png"
+        if not self.is_incheck:
+            self.image = f"img\\{color_num}0{self.type}.png"
+        else:
+            self.image = f"img\\{color_num}0{self.type}c.png"
 
     def get_valid_moves(self, board):
         valid_moves = []
@@ -24,7 +39,7 @@ class King(Piece):
 
         for move in moves:
             if board.move_within_bounds(move):
-                # todo something with self.is_check & self.is_checkmate
+                # todo something with self.check & self.checkmate
                 if not board.cell_is_piece(move):
                     valid_moves.append(move)
                 if (board.cell_is_piece(move)) and board.board[move[0]][move[1]].player != board.turn:
