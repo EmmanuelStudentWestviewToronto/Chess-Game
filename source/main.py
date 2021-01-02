603f78a5909fc30ea955346334674e63a45dd446
RomanJ55 <roman.jaro@web.de>
2021-01-02 19:47:32 +0100

    added some comments

--- source/main.py
+++ source/main.py
@@ -1,7 +1,7 @@
 from ui import ChessUI
 from board import Board
 
-board = Board(900)  # time in seconds
+board = Board(900)  # time in seconds 900/60 = 15 Minutes
 ui = ChessUI(board)
 
 ui.mainloop()
